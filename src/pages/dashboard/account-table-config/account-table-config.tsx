3177fd5c0586ee208b736aa2ae1833e85b6eb6b0
long13579113 <stevencao135@gmail.com>
2024-02-19 16:17:36 +0700

    ui accounts

---
+++ src/pages/dashboard/account-table-config/account-table-config.tsx
@@ -0,0 +1,37 @@
+import { CustomTableConfig } from "src/components/custom-table";
+import { User } from "src/types/user";
+
+const accountTableConfig = (): CustomTableConfig<User["id"], User>[] => [
+  {
+    key: "name",
+    headerLabel: "Họ và tên",
+    type: "string",
+  },
+  {
+    key: "email",
+    headerLabel: "Email",
+    type: "string",
+  },
+  {
+    key: "username",
+    headerLabel: "Tên tài khoản",
+    type: "string",
+  },
+  {
+    key: "password",
+    headerLabel: "Mật khẩu",
+    type: "string",
+  },
+  {
+    key: "id",
+    headerLabel: "Số bài phụ trách",
+    type: "number",
+  },
+  {
+    key: "role",
+    headerLabel: "Phân quyền",
+    type: "string",
+  },
+];
+
+export default accountTableConfig;

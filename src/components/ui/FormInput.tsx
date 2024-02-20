1f5e44f838bbd1fd57cc1d912debd34c2f38cf91
huytinhoc21 <huytinhoc21@gmail.com>
2024-02-20 12:15:21 +0700

    update auth: use form

---
+++ src/components/ui/FormInput.tsx
@@ -0,0 +1,31 @@
+import type { ComponentProps, FC, ReactNode } from "react";
+import { Input, InputProps } from "../shadcn/ui/input";
+import clsx from "clsx";
+
+export interface FormInputProps extends InputProps {
+  error?: boolean;
+  helperText?: ReactNode;
+}
+
+const FormInput: FC<FormInputProps> = ({
+  error,
+  helperText,
+  ...InputProps
+}) => {
+  console.log("error, helperText", error, helperText);
+  return (
+    <>
+      <Input
+        {...InputProps}
+        className={clsx(error && "border-destructive ring-destructive")}
+      />
+      {helperText && (
+        <div className={clsx("text-xs", error && "text-destructive")}>
+          {helperText}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default FormInput;

148dc80ac4ca5fac0ce788942fbbebd882bd3eec
huytinhoc21 <huytinhoc21@gmail.com>
2024-02-27 17:52:32 +0700

    fix: spash screen before auth initialized

--- src/components/ui/SplashScreen.tsx
+++ src/components/ui/SplashScreen.tsx
@@ -1,12 +1,13 @@
-import React from "react";
 import { Logo } from "./Logo";
 
-const SplashScreen: React.FC = () => (
-  <div className="flex items-center justify-center h-screen bg-background-paper fixed top-0 left-0 w-full z-1400">
-    <div className="inline-flex h-48 w-48">
-      <Logo />
+const SplashScreen = () => {
+  return (
+    <div className="fixed top-0 left-0 w-full h-screen z-1400 bg-background flex flex-col items-center justify-center p-10">
+      <div className="w-48 h-48">
+        <Logo />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default SplashScreen;

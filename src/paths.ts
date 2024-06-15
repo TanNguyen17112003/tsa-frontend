486d9125dac9212802d85bf33636be04dc2f47ff
TanNguyen <duytan17112003@gmail.com>
2024-06-16 00:26:10 +0700

    test husky pre-commit

--- src/paths.ts
+++ src/paths.ts
@@ -1,21 +1,21 @@
 export const paths = {
-  index: "/",
+  index: '/',
   auth: {
-    login: "/auth",
-    register: "/auth/register",
-    "forgot-password": "/auth/forgot-password",
-    "reset-password": "/auth/reset-password",
+    login: '/auth',
+    register: '/auth/register',
+    'forgot-password': '/auth/forgot-password',
+    'reset-password': '/auth/reset-password'
   },
   dashboard: {
-    index: "/dashboard",
-    collections: "/dashboard/collections",
-    accounts: "/dashboard/accounts",
-    categories: "/dashboard/categories",
-    reports: "/dashboard/reports",
-    "add-report": "/dashboard/add-report",
-    logout: "/dang-xuat",
+    index: '/dashboard',
+    collections: '/dashboard/collections',
+    accounts: '/dashboard/accounts',
+    categories: '/dashboard/categories',
+    reports: '/dashboard/reports',
+    'add-report': '/dashboard/add-report',
+    logout: '/dang-xuat'
   },
-  401: "/401",
-  404: "/404",
-  500: "/500",
+  401: '/401',
+  404: '/404',
+  500: '/500'
 };

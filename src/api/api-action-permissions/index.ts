4197e2e3a835c736623f643f32d6aff52fd14d2d
TanNguyen <duytan17112003@gmail.com>
2024-08-25 15:15:19 +0700

    refactor: config some files to enable successful bulding

--- src/api/api-action-permissions/index.ts
+++ src/api/api-action-permissions/index.ts
@@ -1,19 +1,17 @@
-import { apiGet, apiPost, apiPut, apiDelete } from "src/utils/api-request";
+import { apiGet, apiPost, apiPut, apiDelete } from 'src/utils/api-request';
 
 export class ApiActionPermissionsApi {
   static async postApiActionPermission(request: {
     permission_id: number;
     api_action_id: number;
   }): Promise<{ id: number }> {
-    return await apiPost("/api_action_permissions", request);
+    return await apiPost('/api_action_permissions', request);
   }
 
   static async getApiActionPermissions(
     request: FormData
-  ): Promise<
-    { api_action_id: number; permission_id: number; api_action_name: string }[]
-  > {
-    const response = await apiGet("/api_action_permissions", request);
+  ): Promise<{ api_action_id: number; permission_id: number; api_action_name: string }[]> {
+    const response = await apiGet('/api_action_permissions', request);
     return response;
   }
 
@@ -21,10 +19,8 @@ export class ApiActionPermissionsApi {
     permission_id: number;
     api_action_ids: number[];
   }): Promise<void> {
-    const response = await apiPut(
-      "/api_action_permissions/all/" + request.permission_id,
-      request
-    );
+    const response = await apiPut('/api_action_permissions/all/' + request.permission_id, request);
+    return response;
   }
 
   static async deleteApiActionPermission(request: {

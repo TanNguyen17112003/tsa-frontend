5e33d89431c2438a6b62502d6a0872bd35f1b6fe
TanNguyen <duytan17112003@gmail.com>
2024-09-03 22:55:48 +0700

    chore: add first page of auth  and refactor footer of landing page

--- src/sections/landing/LandingFooter.tsx
+++ src/sections/landing/LandingFooter.tsx
@@ -1,12 +1,115 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, Stack, Divider, Grid, Link } from '@mui/material';
 import React from 'react';
+import Image from 'next/image';
+import logo from '../../../public/logo.png';
+import { Facebook, Instagram, Youtube } from 'iconsax-react';
+
+interface FooterLinkProps {
+  title: string;
+  link: string;
+  icon?: React.ReactNode;
+}
 
 export const LandingFooter = () => {
+  const aboutUsLinks: FooterLinkProps[] = [
+    {
+      title: 'Nhiệm vụ',
+      link: '#'
+    },
+    {
+      title: 'Đội',
+      link: '#'
+    }
+  ];
+
+  const SupportLinks: FooterLinkProps[] = [
+    {
+      title: 'Liên hệ',
+      link: '#'
+    },
+    {
+      title: 'Chính sách hoàn trả',
+      link: '#'
+    },
+    {
+      title: 'FAQs',
+      link: '#'
+    }
+  ];
+
+  const SocialLinks: FooterLinkProps[] = [
+    {
+      title: 'Instagram',
+      link: '#',
+      icon: <Instagram />
+    },
+    {
+      title: 'Youtube',
+      link: '#',
+      icon: <Youtube />
+    },
+    {
+      title: 'Facebook',
+      link: '#',
+      icon: <Facebook />
+    }
+  ];
   return (
-    <Box bgcolor={'black'} paddingY={2}>
-      <Typography variant={'h6'} textAlign={'center'} color='white'>
-        Copyright © 2024 TSA. All rights reserved.
-      </Typography>
+    <Box bgcolor={'#636e7b'} paddingY={2} color={'white'} paddingX={5}>
+      <Box display={'flex'} paddingBottom={2} justifyContent={'space-around'}>
+        <Image src={logo} alt='logo' width={150} height={150} />
+        <Stack spacing={2} width={'70%'}>
+          <Typography variant='h4' marginBottom={5} color='#5be23d'>
+            Transport Support Application (TSA)
+          </Typography>
+          <Grid container>
+            <Grid item xs={4}>
+              <Typography variant='h6' marginBottom={1}>
+                Về chúng tôi
+              </Typography>
+              <Stack spacing={1}>
+                {aboutUsLinks.map((link, index) => (
+                  <Link key={index} href={link.link} underline='none' color='inherit'>
+                    <Typography variant='body1'>{link.title}</Typography>
+                  </Link>
+                ))}
+              </Stack>
+            </Grid>
+            <Grid item xs={4}>
+              <Typography variant='h6' marginBottom={1}>
+                Hỗ trợ
+              </Typography>
+              <Stack spacing={1}>
+                {SupportLinks.map((link, index) => (
+                  <Link key={index} href={link.link} underline='none' color='inherit'>
+                    <Typography variant='body1'>{link.title}</Typography>
+                  </Link>
+                ))}
+              </Stack>
+            </Grid>
+            <Grid item xs={4}>
+              <Typography variant='h6' marginBottom={1}>
+                Kênh liên lạc
+              </Typography>
+              <Stack spacing={1}>
+                {SocialLinks.map((link, index) => (
+                  <Box display={'flex'} gap={1} key={index}>
+                    {link.icon}
+                    <Link href={link.link} underline='none' color='inherit'>
+                      <Typography variant='body1'>{link.title}</Typography>
+                    </Link>
+                  </Box>
+                ))}
+              </Stack>
+            </Grid>
+          </Grid>
+        </Stack>
+      </Box>
+      <Divider />
+      <Box display={'flex'} justifyContent={'space-between'} paddingTop={2}>
+        <Typography>Copyright © 2024 TSA. All rights reserved.</Typography>
+        <Typography>Terms of Service</Typography>
+      </Box>
     </Box>
   );
 };

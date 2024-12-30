67fe3ad71c76bae717f66edaa7204cd8743d3bb2
TanNguyen <duytan17112003@gmail.com>
2024-12-30 19:13:56 +0700

    refactor: fix for price section in landing

--- src/sections/landing/LandingPrice.tsx
+++ src/sections/landing/LandingPrice.tsx
@@ -8,7 +8,8 @@ interface PriceSectionProps {
 }
 
 const PriceSection: React.FC<PriceSectionProps> = ({ title, items }) => {
-  const { isTablet } = useResponsive();
+  const { isMobile, isTablet } = useResponsive();
+
   return (
     <Stack
       bgcolor={'#f5f5f5'}
@@ -22,7 +23,7 @@ const PriceSection: React.FC<PriceSectionProps> = ({ title, items }) => {
           {title}
         </Typography>
       </Stack>
-      <Stack padding={2} spacing={3} width={isTablet ? '100%' : '100%'}>
+      <Stack padding={2} spacing={isTablet ? 1.5 : 3} width={isTablet ? '100%' : '100%'}>
         {items.map((item, index) => (
           <>
             <Box key={index} display={'flex'} justifyContent={'space-between'}>
@@ -52,7 +53,7 @@ const dormitoryItems = [
 ];
 
 export const LandingPrice = () => {
-  const { isTablet } = useResponsive();
+  const { isMobile, isTablet } = useResponsive();
   return (
     <Box
       color={'black'}
@@ -67,82 +68,98 @@ export const LandingPrice = () => {
       <Typography variant='h4' textAlign={'center'} marginBottom={1} marginTop={5}>
         Bảng giá vận chuyển TSA
       </Typography>
-      <Typography>
-        Đối với mỗi loại hàng hóa, và nơi giao hàng chúng tôi sẽ có mức giá khác nhau.
-      </Typography>
-      <Box
-        display={'flex'}
-        flexDirection={isTablet ? 'column' : 'row'}
-        gap={isTablet ? 3 : 5}
-        justifyContent={'center'}
-        alignItems={'stretch'}
-        mt={1}
-        width={'70%'}
-      >
-        <PriceSection
-          title='Theo khối lượng'
-          items={[
-            { label: 'Dưới 1kg', price: '5.000đ' },
-            { label: 'Từ 1kg đến 3kg', price: '8.000đ' },
-            { label: 'Trên 3kg', price: '12.000đ' }
-          ]}
-        />
-        <PriceSection
-          title='Theo Tầng'
-          items={[
-            { label: 'Từ tầng 1-4', price: '0đ' },
-            { label: 'Từ tầng 4-6', price: '+1.000đ' },
-            { label: 'Trên tầng 6', price: '+2.000đ' }
-          ]}
-        />
-        <Stack
-          bgcolor={'#f5f5f5'}
-          width={isTablet ? '100%' : '50%'}
-          display={isTablet ? 'flex' : ''}
-          flexDirection={isTablet ? 'row' : 'column'}
-          flex={isTablet ? 1 : ''}
-        >
-          <Stack
-            padding={2}
-            className='rounded-t-lg bg-green-400'
-            width={isTablet ? '15%' : 'auto'}
+      {!isMobile && (
+        <Typography>
+          Đối với mỗi loại hàng hóa, và nơi giao hàng chúng tôi sẽ có mức giá khác nhau.
+        </Typography>
+      )}
+      {isMobile && (
+        <p>
+          Vui lòng xem chi tiết{' '}
+          <a
+            target='_blank'
+            href='https://drive.google.com/file/d/18Xr3P7kBUlomNNPPf8Y_3eNThIb_XYJ2/view?usp=drive_link'
+            style={{ color: 'blue', textDecoration: 'underline' }}
           >
-            <Typography variant={isTablet ? 'h6' : 'h5'} textAlign={'center'} color='white'>
-              Theo Kí túc xá
-            </Typography>
-          </Stack>
-          <Stack padding={2} spacing={1.5}>
-            <Stack flexDirection={isTablet ? 'row' : 'column'} gap={2}>
-              <Stack width={isTablet ? '50%' : '100%'}>
-                <Typography variant='h5'>Khu A</Typography>
-                {dormitoryItems.slice(0, 3).map((item, index) => (
-                  <Box key={index} display={'flex'} justifyContent={'space-between'}>
-                    <Typography variant='body1'>{item.label}</Typography>
-                    <Typography variant='body1' fontWeight={'bold'} color='error'>
-                      {item.price}
-                    </Typography>
-                  </Box>
-                ))}
-              </Stack>
-              <Divider
-                orientation={isTablet ? 'vertical' : 'horizontal'}
-                className='border-1 border-blue-700'
-              />
-              <Stack width={isTablet ? '50%' : '100%'}>
-                <Typography variant='h5'>Khu B</Typography>
-                {dormitoryItems.slice(3).map((item, index) => (
-                  <Box key={index} display={'flex'} justifyContent={'space-between'}>
-                    <Typography variant='body1'>{item.label}</Typography>
-                    <Typography variant='body1' fontWeight={'bold'} color='error'>
-                      {item.price}
-                    </Typography>
-                  </Box>
-                ))}
+            tại đây
+          </a>
+        </p>
+      )}
+      {!isMobile && (
+        <Box
+          display={'flex'}
+          flexDirection={isTablet ? 'column' : 'row'}
+          gap={isTablet ? 3 : 5}
+          justifyContent={'center'}
+          alignItems={'stretch'}
+          mt={1}
+          width={'70%'}
+        >
+          <PriceSection
+            title='Theo khối lượng'
+            items={[
+              { label: 'Dưới 1kg', price: '5.000đ' },
+              { label: 'Từ 1kg đến 3kg', price: '8.000đ' },
+              { label: 'Trên 3kg', price: '12.000đ' }
+            ]}
+          />
+          <PriceSection
+            title='Theo Tầng'
+            items={[
+              { label: 'Từ tầng 1-4', price: '0đ' },
+              { label: 'Từ tầng 4-6', price: '+1.000đ' },
+              { label: 'Trên tầng 6', price: '+2.000đ' }
+            ]}
+          />
+          <Stack
+            bgcolor={'#f5f5f5'}
+            width={isTablet ? '100%' : '50%'}
+            display={isTablet ? 'flex' : ''}
+            flexDirection={isTablet ? 'row' : 'column'}
+            flex={isTablet ? 1 : ''}
+          >
+            <Stack
+              padding={2}
+              className='rounded-t-lg bg-green-400'
+              width={isTablet ? '15%' : 'auto'}
+            >
+              <Typography variant={isTablet ? 'h6' : 'h5'} textAlign={'center'} color='white'>
+                Theo Kí túc xá
+              </Typography>
+            </Stack>
+            <Stack padding={2} spacing={1.5}>
+              <Stack flexDirection={isTablet ? 'row' : 'column'} gap={2}>
+                <Stack width={isTablet ? '50%' : '100%'}>
+                  <Typography variant='h5'>Khu A</Typography>
+                  {dormitoryItems.slice(0, 3).map((item, index) => (
+                    <Box key={index} display={'flex'} justifyContent={'space-between'}>
+                      <Typography variant='body1'>{item.label}</Typography>
+                      <Typography variant='body1' fontWeight={'bold'} color='error'>
+                        {item.price}
+                      </Typography>
+                    </Box>
+                  ))}
+                </Stack>
+                <Divider
+                  orientation={isTablet ? 'vertical' : 'horizontal'}
+                  className='border-1 border-blue-700'
+                />
+                <Stack width={isTablet ? '50%' : '100%'}>
+                  <Typography variant='h5'>Khu B</Typography>
+                  {dormitoryItems.slice(3).map((item, index) => (
+                    <Box key={index} display={'flex'} justifyContent={'space-between'}>
+                      <Typography variant='body1'>{item.label}</Typography>
+                      <Typography variant='body1' fontWeight={'bold'} color='error'>
+                        {item.price}
+                      </Typography>
+                    </Box>
+                  ))}
+                </Stack>
               </Stack>
             </Stack>
           </Stack>
-        </Stack>
-      </Box>
+        </Box>
+      )}
     </Box>
   );
 };
